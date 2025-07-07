0f7739f7c131672bf8438d21a70602a2dd2e53c1
Ali Saker <alisaker1999@hotmail.com>
2025-07-07 12:20:32 -0400

    new

--- Program.cs
+++ Program.cs
@@ -13,8 +13,9 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // DbContext
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(
+  options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 // TokenService with Interface
 builder.Services.AddScoped<ITokenService, TokenService>();
